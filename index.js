417ae2308490fcd9e507ce8557db186014d8c524
the-user01 <sakibcom02@gmail.com>
2024-05-13 02:30:27 +0600

    updated Add books page

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 require('dotenv').config();
-app.use(cors());
+
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials: true,
+}));
+
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST')
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+    )
+    next()
+  })
+
 app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8yiviav.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -24,6 +39,20 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
+
+        const bookCollection = client.db('booksDB').collection('books');
+
+        app.get('/allBooks', async(req, res)=>{
+            const result = await bookCollection.find().toArray();
+            res.send(result);
+        })
+
+        app.post('/allBooks', async(req, res)=>{
+            const newBooks = req.body;
+            const result = await bookCollection.insertOne(newBooks);
+
+            res.send(result);
+        })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });

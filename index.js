73f6b461583e6ea5c81b5893784c3312ed286a20
the-user01 <sakibcom02@gmail.com>
2024-05-13 18:52:17 +0600

    added put server connection

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,10 +8,10 @@ const port = process.env.PORT || 5000;
 require('dotenv').config();
 
 
-const corsOptions ={
-    origin:['http://localhost:5173'], 
-    credentials:true,
-    optionSuccessStatus:200
+const corsOptions = {
+    origin: ['http://localhost:5173'],
+    credentials: true,
+    optionSuccessStatus: 200
 }
 app.use(cors(corsOptions));
 
@@ -19,11 +19,11 @@ app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST')
     res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept'
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
     )
     next();
-  })
+})
 
 app.use(express.json());
 
@@ -43,16 +43,47 @@ async function run() {
 
         const bookCollection = client.db('booksDB').collection('books');
 
-        app.get('/all-books', async(req, res)=>{
+        app.get('/all-books', async (req, res) => {
             const result = await bookCollection.find().toArray();
             res.send(result);
         })
-        app.post('/all-books', async(req, res)=>{
+
+        app.get('/all-books/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await bookCollection.findOne(query);
+            res.send(result);
+        })
+
+        app.post('/all-books', async (req, res) => {
             const newBooks = req.body;
             const result = await bookCollection.insertOne(newBooks);
 
             res.send(result);
         })
+
+        app.put('/all-books/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const options = { upsert: true };
+
+            const updateBook = req.body;
+            const book = {
+                $set: {
+                    book_name: updateBook.book_name,
+                    book_quantity: updateBook.book_quantity,
+                    author_name: updateBook.author_name,
+                    category: updateBook.category,
+                    rating: updateBook.rating,
+                    description: updateBook.description,
+                    photo: updateBook.photo,
+                }
+            }
+
+            const result = await bookCollection.updateOne(filter, book, options);
+            res.send(result) 
+        })
+
     } finally {
         // Ensures that the client will close when you finish/error
     }

33144302c55437ab1730193937447c5986f5fcb7
the-user01 <sakibcom02@gmail.com>
2024-05-15 21:33:15 +0600

    added jwt

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,7 +11,7 @@ require('dotenv').config();
 
 
 const corsOptions = {
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', 'https://book-mania-c.web.app', 'https://book-mania-c.firebaseapp.com'],
     credentials: true,
     optionSuccessStatus: 200
 }
@@ -26,6 +28,7 @@ app.use(function (req, res, next) {
 })
 
 app.use(express.json());
+app.use(cookieParser())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8yiviav.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -38,8 +41,51 @@ const client = new MongoClient(uri, {
     }
 });
 
+const verifyToken = async (req, res, next) => {
+    const token = req.cookies?.token;
+
+    if (!token) {
+        return res.status(401).send({ message: Unauthrized })
+    }
+    jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ message: "Unauthorized" });
+        }
+        req.user = decoded;
+        next();
+    })
+}
+
+
+const cookieOption = {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "None" : "Strict",
+}
+
+
 async function run() {
     try {
+
+        // Auth related api
+
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1hr' });
+
+            res.cookie('token', token, cookieOption)
+                .send({ success: true });
+        })
+
+        app.post('/logout', async(req, res)=>{
+            const user = req.body;
+            console.log('loging out', user);
+
+            res
+            .clearCookie('token', {...cookieOption ,maxAge: 0})
+            .send({success: true})
+        })
+
 
         const bookCollection = client.db('booksDB').collection('books');
 
@@ -55,9 +101,9 @@ async function run() {
             res.send(result);
         })
 
-        app.get('/all-books/category/:category', async(req, res)=>{
+        app.get('/all-books/category/:category', async (req, res) => {
             const category = req.params.category;
-            const cursor = {category: category};
+            const cursor = { category: category };
             const options = { upsert: true };
             const result = await bookCollection.find(cursor, options).toArray();
 
@@ -90,7 +136,7 @@ async function run() {
             }
 
             const result = await bookCollection.updateOne(filter, book, options);
-            res.send(result) 
+            res.send(result)
         })
 
 
@@ -99,10 +145,10 @@ async function run() {
 
         const categoryCollection = client.db('booksDB').collection('booksCategory');
 
-        app.get('/books-category', async(req, res)=>{
+        app.get('/books-category', async (req, res) => {
             const bookCategory = req.body;
             const result = await categoryCollection.find(bookCategory).toArray();
-            
+
             res.send(result);
         })
 
@@ -111,22 +157,22 @@ async function run() {
 
         const borrowedBooksCollection = client.db('booksDB').collection('borrowedBooks');
 
-        app.get('/borrowed-books', async(req, res)=>{
+        app.get('/borrowed-books', verifyToken, async (req, res) => {
             const borrowedBooks = req.body;
             const result = await borrowedBooksCollection.find(borrowedBooks).toArray();
 
             res.send(result);
         })
 
-        app.get('/borrowed-books/email/:email', async(req, res)=>{
+        app.get('/borrowed-books/email/:email', async (req, res) => {
             const email = req.params.email;
-            const cursor = {user_email: email};
+            const cursor = { user_email: email };
             const result = await borrowedBooksCollection.find(cursor).toArray();
 
             res.send(result);
         })
 
-        app.post('/borrowed-books', async(req, res)=>{
+        app.post('/borrowed-books', async (req, res) => {
             const newBorrowedBooks = req.body;
 
             const result = await borrowedBooksCollection.insertOne(newBorrowedBooks);
